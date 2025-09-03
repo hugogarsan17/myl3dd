9d9c12f3b400cf9f8acebecc0f580c671b710ca3
Hugo GARCIA <hugo.gds2004@gmail.com>
2025-09-03 17:32:03 +0200

    slug

--- src/app/servicios/Carteleria-digital/page.tsx
+++ src/app/servicios/Carteleria-digital/page.tsx
@@ -1,7 +1,7 @@
 import CarteleriaDigital from "@/app/Components/carteleria-digital/carteleria-digital";
 import SiteHeader from "../../Components/site-header/Siteheader";
 
-export default function ContactPage() {
+export default function CarteleriaDigitalPage() {
   return (
     <>
       <SiteHeader logoSrc="/logo.png" logoAlt="Nombre de la marca" />
---
+++ src/app/servicios/educacion/page.tsx
@@ -0,0 +1,10 @@
+import SiteHeader from "../../Components/site-header/Siteheader";
+import Educacion from "@/app/Components/educacion/educacion";
+export default function EducacionPage() {
+  return (
+    <>
+      <SiteHeader logoSrc="/logo.png" logoAlt="Nombre de la marca" />
+      <Educacion />
+    </>
+  );
+}
