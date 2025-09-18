4117ed5f5f7960c82a19b1758609b0a84478c972
Hugo GARCIA <hugo.garcia@socotec.com>
2025-09-18 17:04:00 +0200

    speed vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Home from "./Components/Home/Home";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Page() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <SpeedInsights />
+    </>
+  );
 }
