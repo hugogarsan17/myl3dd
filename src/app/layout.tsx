53574f069146199189b247174689b16f27739b04
Hugo GARCIA <hugo.garcia@socotec.com>
2025-09-25 13:09:51 +0200

    ERRORES CORREGIDOS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"; // 👈 Importa aquí
 import "./globals.css";
+import "./Components/Home/Home.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,7 +33,7 @@ export const metadata: Metadata = {
     images: ["/og.jpg"],
   },
   robots: { index: true, follow: true },
-  icons: { icon: "/MYL3D.png" },
+  icons: { icon: "/favicon.ico" },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
